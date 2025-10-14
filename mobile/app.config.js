bf921fc9fa2d572de22dc128c7cd7dc3f7a20b34
Matt Henry <matt@innovativelanguage.com>
2025-10-14 16:09:54 -0600

    Remove ios folder from repo

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -15,7 +15,7 @@ module.exports = {
     ios: {
       icon: './assets/icon.png',
       supportsTablet: true,
-      bundleIdentifier: 'com.fancymatt.swccgmobile',
+      bundleIdentifier: 'com.fancymatt.swccg',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
       },
@@ -26,7 +26,7 @@ module.exports = {
         backgroundColor: '#ffffff',
       },
       edgeToEdgeEnabled: true,
-      package: 'com.fancymatt.swccgmobile',
+      package: 'com.fancymatt.swccg',
       // Enable auto backup with custom rules
       allowBackup: true,
     },
@@ -38,17 +38,6 @@ module.exports = {
         projectId: '8bf2324b-7fd0-404d-a407-603a4ca5db20',
       },
     },
-    plugins: [
-      // Custom plugin to add fullBackupContent to AndroidManifest
-      function withAndroidBackupRules(config) {
-        const { withAndroidManifest } = require('@expo/config-plugins');
-
-        return withAndroidManifest(config, (config) => {
-          const mainApplication = config.modResults.manifest.application[0];
-          mainApplication.$['android:fullBackupContent'] = '@xml/backup_rules';
-          return config;
-        });
-      },
-    ],
+    plugins: [],
   },
 };
